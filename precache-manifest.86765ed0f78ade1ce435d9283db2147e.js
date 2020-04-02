fd4da2d56e32c0971ff0b63c03d75a9824b227c8
Vahagn Sargsyan <vahagnsargsyan@192-168-1-113.tpgi.com.au>
2020-04-02 15:41:34 +1100

    Updates

---
+++ precache-manifest.86765ed0f78ade1ce435d9283db2147e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3bcf1f89b7037db420fb8217df004e",
+    "url": "/how-long-does-vic-have/index.html"
+  },
+  {
+    "revision": "6f458911846e306b900c",
+    "url": "/how-long-does-vic-have/static/css/main.4e8b7aa5.chunk.css"
+  },
+  {
+    "revision": "a4c7583f3f23d6b36e14",
+    "url": "/how-long-does-vic-have/static/js/2.edd8a5b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/how-long-does-vic-have/static/js/2.edd8a5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f458911846e306b900c",
+    "url": "/how-long-does-vic-have/static/js/main.b421b7f5.chunk.js"
+  },
+  {
+    "revision": "70701e632ec4b0628870",
+    "url": "/how-long-does-vic-have/static/js/runtime-main.5c84e4c9.js"
+  },
+  {
+    "revision": "e4fdb8e7ca20bbb8f89f792124c967d3",
+    "url": "/how-long-does-vic-have/static/media/clockface.e4fdb8e7.png"
+  },
+  {
+    "revision": "fb5f536cb6ed2435c70d027929b6eb13",
+    "url": "/how-long-does-vic-have/static/media/hourhand.fb5f536c.png"
+  },
+  {
+    "revision": "e33f41eee0e3a9038a1d87e74d822d02",
+    "url": "/how-long-does-vic-have/static/media/minhand.e33f41ee.png"
+  },
+  {
+    "revision": "2804fed367f9b438f5f0eefa0faa4671",
+    "url": "/how-long-does-vic-have/static/media/sechand.2804fed3.png"
+  }
+]);
